21ba6e0c87d034c0774e50209b1428dcef04c421
Barrellon Sylvain <sylvain.barrellon@hotmail.fr>
2023-03-28 14:26:02 +0200

    modif des rythmbars en js avec mouseover du logo

---
+++ public/js/rythm-old.js
@@ -0,0 +1,114 @@
+function rythmicBars() {
+  
+    anime({
+      targets: '.rythm-bar-1',
+      keyframes: [
+        { height: '150px' },
+        { height: '0' },
+        { height: '90px' },
+        { height: '0' },
+        { height: '50px' },
+        { height: '0' },
+        { height: '170px' },
+        { height: '0' },
+        { height: '70px' },
+        { height: '0' },
+        { height: '40px' },
+        { height: '0' }
+      ],
+      easing: 'easeOutElastic(1, .8)',
+      direction: 'alternate',
+      loop: true,
+      duration: 2500
+    });
+
+    anime({
+      targets: '.rythm-bar-2',
+      keyframes: [
+        { height: '80px' },
+        { height: '0' },
+        { height: '230px' },
+        { height: '0' },
+        { height: '180px' },
+        { height: '0' },
+        { height: '90px' },
+        { height: '0' },
+        { height: '200px' },
+        { height: '0' },
+        { height: '150px' },
+        { height: '0' }
+      ],
+      easing: 'easeOutElastic(1, .8)',
+      direction: 'alternate',
+      loop: true,
+      duration: 2500,
+    });
+
+    anime({
+      targets: '.rythm-bar-3',
+      keyframes: [
+        { height: '180px' },
+        { height: '0' },
+        { height: '260px' },
+        { height: '0' },
+        { height: '90px' },
+        { height: '0' },
+        { height: '150px' },
+        { height: '0' },
+        { height: '290px' },
+        { height: '0' },
+        { height: '110px' },
+        { height: '0' }
+      ],
+      easing: 'easeOutElastic(1, .8)',
+      direction: 'alternate',
+      loop: true,
+      duration: 2500,
+    });
+
+    anime({
+      targets: '.rythm-bar-4',
+      keyframes: [
+        { height: '100px' },
+        { height: '0' },
+        { height: '200px' },
+        { height: '0' },
+        { height: '60px' },
+        { height: '0' },
+        { height: '120px' },
+        { height: '0' },
+        { height: '170px' },
+        { height: '0' },
+        { height: '80px' },
+        { height: '0' }
+      ],
+      easing: 'easeOutElastic(1, .8)',
+      direction: 'alternate',
+      loop: true,
+      duration: 2500,
+    });
+
+    anime({
+      targets: '.rythm-bar-5',
+      keyframes: [
+        { height: '60px' },
+        { height: '0' },
+        { height: '120px' },
+        { height: '0' },
+        { height: '30px' },
+        { height: '0' },
+        { height: '90px' },
+        { height: '0' },
+        { height: '140px' },
+        { height: '0' },
+        { height: '50px' },
+        { height: '0' }
+      ],
+      easing: 'easeOutElastic(1, .8)',
+      direction: 'alternate',
+      loop: true,
+      duration: 2500,
+    });
+}
+  
+
--- public/js/rythm.js
+++ public/js/rythm.js
@@ -1,79 +1,85 @@
-anime({
-    targets: '.rythm-bar-1',
-    keyframes: [
-        {height: '150px'},
-        {height: '0'},
-        {height: '90px'},
-        {height: '0'},
-        {height: '50px'},
-        {height: '0'}
-    ],
-    easing: 'easeOutElastic(1, .8)',
-    direction: 'alternate',
-    loop: true,
-    duration: 1200
-  });
+let logo = document.querySelector("#logo");
+let isAnimated = false;
+document.querySelector('.rythm-bar-1').style.height = "0";
+let firstBar = document.querySelector('.rythm-bar-1').style.height;
+document.querySelector('.rythm-bar-2').style.height = "0";
+let secondBar = document.querySelector('.rythm-bar-2').style.height;
+document.querySelector('.rythm-bar-3').style.height = "0";
+let thirdBar = document.querySelector('.rythm-bar-3').style.height;
+document.querySelector('.rythm-bar-4').style.height = "0";
+let fourthBar = document.querySelector('.rythm-bar-4').style.height;
+document.querySelector('.rythm-bar-5').style.height = "0";
+let fifthBar = document.querySelector('.rythm-bar-5').style.height;
+let tempo = getRandomArbitrary(150, 300)
 
-  anime({
-    targets: '.rythm-bar-2',
-    keyframes: [
-        {height: '80px'},
-        {height: '0'},
-        {height: '230px'},
-        {height: '0'},
-        {height: '180px'}
-    ],
-    easing: 'easeOutElastic(1, .8)',
-    direction: 'alternate',
-    loop: true,
-    duration: 1500,
-  });
+logo.addEventListener('mouseover', function () {
+    isAnimated = true;
+    
+});
+logo.addEventListener('mouseout', function () {
+    isAnimated = false;
+});
 
-  anime({
-    targets: '.rythm-bar-3',
-    keyframes: [
-        {height: '260px'},
-        {height: '0'},
-        {height: '180px'},
-        {height: '0'},
-        {height: '90px'},
-        {height: '0'}
-    ],
-    easing: 'easeOutElastic(1, .8)',
-    direction: 'alternate',
-    loop: true,
-    duration: 2000,
-  });
 
-  anime({
-    targets: '.rythm-bar-4',
-    keyframes: [
-        {height: '200px'},
-        {height: '0'},
-        {height: '100px'},
-        {height: '0'},
-        {height: '60px'},
-        {height: '0'}
-    ],
-    easing: 'easeOutElastic(1, .8)',
-    direction: 'alternate',
-    loop: true,
-    duration: 1500,
-  });
 
-  anime({
-    targets: '.rythm-bar-5',
-    keyframes: [
-        {height: '120px'},
-        {height: '0'},
-        {height: '60px'},
-        {height: '0'},
-        {height: '30px'},
-        {height: '0'}
-    ],
-    easing: 'easeOutElastic(1, .8)',
-    direction: 'alternate',
-    loop: true,
-    duration: 1800,
-  });
-  
+function animateBars(){
+
+    if (isAnimated) {
+        if (firstBar == "0px") {
+            firstBar = parseInt(getRandomArbitrary(50, 200)).toString() + "px";
+            document.querySelector('.rythm-bar-1').style.height = firstBar;
+            document.querySelector('.rythm-bar-10').style.height = firstBar;
+            secondBar = parseInt(getRandomArbitrary(100, 250)).toString() + "px";
+            document.querySelector('.rythm-bar-2').style.height = secondBar;
+            document.querySelector('.rythm-bar-9').style.height = secondBar;
+            thirdBar = parseInt(getRandomArbitrary(150, 300)).toString() + "px";
+            document.querySelector('.rythm-bar-3').style.height = thirdBar;
+            document.querySelector('.rythm-bar-8').style.height = thirdBar;
+            fourthBar = parseInt(getRandomArbitrary(100, 250)).toString() + "px";
+            document.querySelector('.rythm-bar-4').style.height = fourthBar;
+            document.querySelector('.rythm-bar-7').style.height = fourthBar;
+            fifthBar = parseInt(getRandomArbitrary(50, 200)).toString() + "px";
+            document.querySelector('.rythm-bar-5').style.height = fifthBar;
+            document.querySelector('.rythm-bar-6').style.height = fifthBar;
+
+        }
+        else {
+            firstBar = "0px";
+            document.querySelector('.rythm-bar-1').style.height = firstBar;
+            document.querySelector('.rythm-bar-10').style.height = firstBar;
+            secondBar = "0px";
+            document.querySelector('.rythm-bar-2').style.height = secondBar
+            document.querySelector('.rythm-bar-9').style.height = secondBar;
+            thirdBar = "0px";
+            document.querySelector('.rythm-bar-3').style.height = thirdBar;
+            document.querySelector('.rythm-bar-8').style.height = thirdBar;
+            fourthBar = "0px";
+            document.querySelector('.rythm-bar-4').style.height = fourthBar;
+            document.querySelector('.rythm-bar-7').style.height = fourthBar;
+            fifthBar = "0px";
+            document.querySelector('.rythm-bar-5').style.height = fifthBar;
+            document.querySelector('.rythm-bar-6').style.height = fifthBar;
+        }
+    }
+
+
+    clearInterval(timer);
+    timer = setInterval(animateBars,parseInt(getRandomArbitrary(80,250)))
+}
+
+    
+    
+    
+let timer = setInterval(animateBars,150);
+
+
+
+
+
+
+
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
